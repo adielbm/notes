d0c536e401d4966a27378c95909fcabe9b81607f
adielBm <adiel.bm5@gmail.com>
2025-03-17 13:27:20 +0200

    misc

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
     analytics: null,
     locale: "en-US",
     baseUrl: "adiel.netlify.app",
-    ignorePatterns: ["private", "templates", ".obsidian", "**/drafts", "**/misc", "My Tasks.md"],
+    ignorePatterns: ["private", "templates", ".obsidian", "**/drafts", "My Tasks.md"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
