11cdbd29965009c8caeb31ab3a5243c5b715c59f
adielBm <adiel.bm5@gmail.com>
2025-03-17 14:19:49 +0200

    css

--- quartz/components/ArticleTitle.tsx
+++ quartz/components/ArticleTitle.tsx
@@ -10,10 +10,4 @@ const ArticleTitle: QuartzComponent = ({ fileData, displayClass }: QuartzCompone
   }
 }
 
-ArticleTitle.css = `
-.article-title {
-  margin: 2rem 0 0 0;
-}
-`
-
 export default (() => ArticleTitle) satisfies QuartzComponentConstructor
